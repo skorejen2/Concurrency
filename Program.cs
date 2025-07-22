ff34a0d4de5b8c830d7c3a84c069735c2736235a
Skorejen <m.ciebien12@gmail.com>
2025-07-22 13:58:32 +0200

    add everything

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿
+
+
+
+using System.Collections.Concurrent;
+
+public class Counter
+{
+    public static BlockingCollection<int> totalValues = new();
+
+    static void Main(string[] args)
+    {
+        Thread t1 = new(() => Counter.InsertOne());
+        t1.Name = "Thread 1";
+
+        Thread t2 = new(() => Counter.TakeOne());
+        t2.Name = "Thread 2";
+
+        Thread t3 = new(() =>
+        {
+            for (var i = 0; ; i++)
+            {
+                System.Console.WriteLine("Total values count: " + totalValues.Count);
+                Thread.Sleep(5000);
+            }
+        });
+
+        t1.Start();
+        t2.Start();
+        t3.Start();
+
+        t1.Join();
+        t2.Join();
+        foreach (var item in totalValues)
+        {
+            System.Console.WriteLine(item + "'\n");
+        }
+
+        Console.ReadLine();
+    }
+
+    public delegate void CountTo20();
+    public static void InsertOne()
+    {
+        for (var a = 0; a < 1000; a++)
+        {
+            var i = new Random().Next();
+            totalValues.Add(i);
+            System.Console.WriteLine(Thread.CurrentThread.Name + " added value " + i);
+            Thread.Sleep(50);
+        }
+        
+    }
+    public static void TakeOne()
+    {
+        for (int a = 0; ; a++)
+        {
+            var i = totalValues.Take();
+            System.Console.WriteLine(Thread.CurrentThread.Name + " took value " + i);
+            Thread.Sleep(200);
+        }
+        
+    }
+
+}
+
+public class Producer
+{
+
+
+
+}
+
+public class Receiver
+{
+
+}
